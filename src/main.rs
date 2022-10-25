ff3c12c785663098c72423f1547293cac72df2b9
Oussama Rahmouni <orahmoun@e2r3p4.1337.ma>
2022-10-25 13:43:48 +0100

    working currently

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![allow(unused)]
 
 use std::env::args;
+use std::sync::atomic::AtomicI8;
+use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex, MutexGuard};
 use std::time::{Duration, Instant};
 use std::{process, thread};
@@ -25,133 +27,123 @@ impl ForksPool {
     }
 
     fn take_fork(fork: &Arc<Mutex<usize>>) -> MutexGuard<usize> {
+        let new_fork = Arc::clone(fork);
         let mut guard = fork.lock().unwrap();
         *guard += 1;
         guard
     }
 
-    fn drop_fork(fork_guard: &Arc<Mutex<usize>>) {
+    fn drop_fork(fork_guard: MutexGuard<usize>) {
         drop(fork_guard)
     }
 }
 
 #[derive(Debug, Default)]
 struct Instructions {
-    time_to_sleep: Duration,
-    time_to_eat: Duration,
     time_to_die: Duration,
+    time_to_eat: Duration,
+    time_to_sleep: Duration,
 }
 
 impl Instructions {
-    fn new(ttd: &str, tte: &str, tts: &str) -> Result<Self, String> brew install node{
+    fn new(ttd: &str, tte: &str, tts: &str) -> Result<Self, String> {
+        let ttd: u64 = match ttd.parse() {
+            Ok(ttd) => ttd,
+            _ => return Err("error parsing time to die to A integer".to_string()),
+        };
         let tts: u64 = match tts.parse() {
             Ok(tts) => tts,
             _ => return Err("error parsing time to sleep to A integer".to_string()),
         };
         let tte: u64 = match tte.parse() {
-            Ok(tts) => tts,
+            Ok(tte) => tte,
             _ => return Err("error parsing time to eat to A integer".to_string()),
         };
-        let ttd: u64 = match ttd.parse() {
-            Ok(tts) => tts,
-            _ => return Err("error parsing time to die to A integer".to_string()),
-        };
         Ok(Instructions {
-            time_to_sleep: Duration::from_millis(tts),
-            time_to_eat: Duration::from_millis(tte),
             time_to_die: Duration::from_millis(ttd),
+            time_to_eat: Duration::from_millis(tte),
+            time_to_sleep: Duration::from_millis(tts),
         })
     }
 }
 
+fn safe_print<'a>(action: &'a str, id: usize, start_time: Instant, mutex: &'a Arc<Mutex<usize>>) {
+    let print_guard = mutex.lock().unwrap();
+    println!(
+        "{} philo {} {action}",
+        start_time.elapsed().as_millis(),
+        id + 1
+    );
+}
+
+fn update_time(time: &Arc<Mutex<Instant>>) {
+    let mut guard = time.lock().unwrap();
+    *guard = Instant::now();
+}
+
 type MInstant = Arc<Mutex<Instant>>;
 
-// #[derive(Debug, Clone)]
-// struct Philo {
-//     id: usize,
-//     last_meal: MInstant,
-// }
-
-// impl Philo {
-//     fn new(id: usize, last_meal: Instant) -> Self {
-//         Philo {
-//             id,
-//             last_meal: Arc::new(Mutex::new(last_meal)),
-//         }
-//     }
-
-//     fn last_meal(&self) -> MInstant {
-//         Arc::clone(&self.last_meal)
-//     }
-// }
-
 fn main() {
-    let start = Instant::now();
-
-    // thread::sleep(Duration::from_secs(2));
-    // println!("{} s", start.elapsed().as_millis());
-
     let args = args().collect::<Vec<String>>();
     if args.len() != 5 {
-        return println!("wrong nums of args");
+        return println!("wrong nums of args {{num, die, eat, sleep}}");
     };
     let nums_of_philos = match args[1].parse() {
         Ok(nums_of_philos) => nums_of_philos,
         _ => return println!("error parsing nums of philos to A integer"),
     };
-    let time_to_die = &args[2];
-    let time_to_eat = &args[3];
-    let time_to_sleep = &args[4];
 
-    let forks = ForksPool::init(nums_of_philos);
-
-    let instructions = match Instructions::new(&time_to_sleep, &time_to_eat, &time_to_die) {
+    let instructions = match Instructions::new(&args[2], &args[3], &args[4]) {
         Ok(instructions) => instructions,
         Err(error) => return println!("{error}"),
     };
 
-    let mut handlers = Vec::new();
-    (0..nums_of_philos).for_each(|id| {
-        let fork1 = forks.get(id);
-        let fork2 = forks.get((id + 1) % nums_of_philos);
-        handlers.push(thread::spawn(move || {
-            if id % 2 == 0 {
-                thread::sleep(Duration::from_millis(2000));
-            }
-            // let last_meal = Arc::new(Mutex::new(Instant::now()));
-            // let last_meal_checker = Arc::clone(&last_meal);
-            // let checker = thread::spawn(move || loop {
-            //     if last_meal_checker.lock().unwrap().elapsed() > instructions.time_to_die {
-            //         thread::sleep(Duration::from_millis(100));
-            //         println!("dead {id}");
-            //         process::exit(1);
-            //     }
-            // });
+    let (tx, rx) = channel();
+    let forks = ForksPool::init(nums_of_philos);
+    let p_mutex = Arc::new(Mutex::new(0));
+    let start_time = Instant::now();
+    let handlers: Vec<_> = (0..nums_of_philos)
+        .map(|id| {
+            let fork1 = forks.get(id);
+            let fork2 = forks.get((id + 1) % nums_of_philos);
+            let p_mutex_checker = Arc::clone(&p_mutex);
+            let p_mutex = Arc::clone(&p_mutex);
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let last_meal = Arc::new(Mutex::new(Instant::now()));
+                let last_meal_checker = Arc::clone(&last_meal);
+                if id % 2 == 0 {
+                    thread::sleep(Duration::from_millis(5));
+                }
+                thread::spawn(move || loop {
+                    thread::sleep(Duration::from_millis(5));
+                    if last_meal_checker.lock().unwrap().elapsed() > instructions.time_to_die {
+                        let p_guard = p_mutex_checker.lock().unwrap();
+                        println!("{} philo {id} is dead ", start_time.elapsed().as_millis());
+                        tx.send(()).expect("Could not send signal on channel.");
+                        loop {
+                            thread::sleep(Duration::from_secs(u64::MAX));
+                        }
+                    }
+                });
 
-            // let last_meal_philo = Arc::clone(&last_meal);
-            loop {
-                let guard1 = ForksPool::take_fork(&fork1);
-                println!("philo {} took a left fork", id + 1);
-                let guard2 = ForksPool::take_fork(&fork2);
-                println!("philo {} took a right fork", id + 1);
-                // *last_meal_philo.lock().unwrap() = Instant::now();
+                loop {
+                    let guard1 = ForksPool::take_fork(&fork1);
+                    safe_print("took a left fork", id, start_time, &p_mutex);
+                    let guard2 = ForksPool::take_fork(&fork2);
+                    safe_print("took a right fork", id, start_time, &p_mutex);
+                    update_time(&last_meal);
+                    safe_print("is eating", id, start_time, &p_mutex);
+                    thread::sleep(instructions.time_to_eat);
+                    ForksPool::drop_fork(guard1);
+                    ForksPool::drop_fork(guard2);
+                    safe_print("is sleeping", id, start_time, &p_mutex);
+                    thread::sleep(instructions.time_to_sleep);
+                    safe_print("is thinking", id, start_time, &p_mutex);
+                }
+            })
+        })
+        .collect();
 
-                println!("philo {} is eating", id + 1);
-                thread::sleep(instructions.time_to_eat);
-                ForksPool::drop_fork(&fork1);
-                ForksPool::drop_fork(&fork2);
-                println!("philo {} is sleeping", id + 1);
-                thread::sleep(instructions.time_to_sleep);
-                println!("philo {} is thinking", id + 1);
-                // println!(
-                //     "{id} {}",
-                //     last_meal_philo.lock().unwrap().elapsed().as_millis()
-                // );
-            }
-        }))
-    });
-
-    for handler in handlers {
-        handler.join().unwrap();
-    }
+    rx.recv().expect("Could not receive from channel.");
 }
