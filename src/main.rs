db71d5519ded1550d983c17536e0c13a12953bad
Oussama Rahmouni <orahmoun@e1r3p7.1337.ma>
2022-10-27 12:53:38 +0100

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,9 @@ impl ForksPool {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 struct Instructions {
+    nums_of_philos: usize,
     time_to_die: Duration,
     time_to_eat: Duration,
     time_to_sleep: Duration,
@@ -49,24 +50,25 @@ fn string_to<T: FromStr>(target: &str, err: &str) -> Result<T, String> {
 }
 
 impl Instructions {
-    fn new(ttd: &str, tte: &str, tts: &str, must_eat: Option<&String>) -> Result<Self, String> {
-        let ttd = string_to(ttd, "error parsing time to die")?;
-        let tte = string_to(tte, "error parsing time to sleep")?;
-        let tts = string_to(tts, "error parsing time to eat")?;
-
-        let must_eat = match must_eat {
-            Some(value) => match string_to(value, "error parsing must_eat") {
-                Ok(must_eat) => Some(must_eat),
-                Err(err) => return Err(err),
-            },
-            _ => None,
-        };
-
+    fn new(
+        nums_of_philos: &str,
+        ttd: &str,
+        tte: &str,
+        tts: &str,
+        must_eat: Option<&String>,
+    ) -> Result<Self, String> {
         Ok(Instructions {
-            time_to_die: Duration::from_millis(ttd),
-            time_to_eat: Duration::from_millis(tte),
-            time_to_sleep: Duration::from_millis(tts),
-            must_eat,
+            nums_of_philos: string_to(nums_of_philos, "error parsing nums of philos")?,
+            time_to_die: Duration::from_millis(string_to(ttd, "error parsing time to die")?),
+            time_to_eat: Duration::from_millis(string_to(tte, "error parsing time to sleep")?),
+            time_to_sleep: Duration::from_millis(string_to(tts, "error parsing time to eat")?),
+            must_eat: match must_eat {
+                Some(value) => match string_to(value, "error parsing must_eat") {
+                    Ok(must_eat) => Some(must_eat),
+                    Err(err) => return Err(err),
+                },
+                _ => None,
+            },
         })
     }
 }
@@ -85,32 +87,27 @@ fn update_time(time: &MInstant) {
 }
 
 fn main() {
-    let args = args().collect::<Vec<String>>();
+    let args: Vec<String> = args().collect();
 
     match args.len() {
         5 | 6 => (),
         _ => return println!("wrong nums of args {{num, die, eat, sleep}}"),
     };
 
-    let nums_of_philos = match args[1].parse() {
-        Ok(nums_of_philos) => nums_of_philos,
-        _ => return println!("error parsing nums of philos to A integer"),
-    };
-
-    let instructions = match Instructions::new(&args[2], &args[3], &args[4], args.get(5)) {
+    let instructions = match Instructions::new(&args[1], &args[2], &args[3], &args[4], args.get(5))
+    {
         Ok(instructions) => instructions,
         Err(error) => return println!("{error}"),
     };
 
     let (tx, rx) = channel();
-    let forks = ForksPool::init(nums_of_philos);
+    let forks = ForksPool::init(instructions.nums_of_philos);
     let p_mutex = Arc::new(Mutex::new(0));
     let must_eat_rec = Arc::new(Mutex::new(0));
     let start_time = Instant::now();
-    (0..nums_of_philos).for_each(|id| {
+    (0..instructions.nums_of_philos).for_each(|id| {
         let fork1 = forks.get(id);
-        let fork2 = forks.get((id + 1) % nums_of_philos);
-        let p_mutex_checker = Arc::clone(&p_mutex);
+        let fork2 = forks.get((id + 1) % instructions.nums_of_philos);
         let p_mutex = Arc::clone(&p_mutex);
         let must_eat_rec = Arc::clone(&must_eat_rec);
         let tx = tx.clone();
@@ -121,13 +118,14 @@ fn main() {
 
             let last_meal = Arc::new(Mutex::new(Instant::now()));
             let last_meal_checker = Arc::clone(&last_meal);
+            let p_mutex_checker = Arc::clone(&p_mutex);
             let must_eat_rec_checker = Arc::clone(&must_eat_rec);
             let mut philo_eat = 0;
 
             thread::spawn(move || loop {
                 thread::sleep(Duration::from_millis(5));
                 if instructions.must_eat.is_some()
-                    && *must_eat_rec_checker.lock().unwrap() == nums_of_philos
+                    && *must_eat_rec_checker.lock().unwrap() == instructions.nums_of_philos
                 {
                     let _print_guard = p_mutex_checker.lock().unwrap();
                     tx.send(()).expect("Could not send signal on channel.");
